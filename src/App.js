33bc7b152c8675ada21311e5df2dd0b19b32f4c3
gaoshuai <19993440500@163.com>
2021-05-24 17:45:00 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Header from "./component/Header"
+import {Route, Switch} from 'react-router-dom'
+import MyNavLink from "./component/MyNavLink";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            {/*<a className="list-group-item active" href="./about.html">About</a>*/}
+                            {/*<a className="list-group-item" href="./home.html">Home</a>*/}
+                            <MyNavLink to="/about">About</MyNavLink>
+                            <MyNavLink to="/Home">Home</MyNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                <Switch>
+                                    <Route path='/playergs/about' component={About}/>
+                                    <Route path='/playergs/home' component={Home}/>
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/component/Header/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <div className="page-header"><h2>React Router Demo</h2></div>
+            </div>
+        );
+    }
+}
+
+export default Index;
